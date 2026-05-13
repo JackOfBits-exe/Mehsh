9a66f39f14b90c9a880c50123164664ba4e8df73
Prav Prateek <prateekprav@gmail.com>
2026-05-13 09:54:25 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <unistd.h>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -20,8 +22,22 @@ int main() {
     else if(command.substr(0,5)=="type ")
     {
       std::string arg=command.substr(5);
-      if(arg=="echo" || arg=="exit" || arg=="type") std::cout<<arg<<" is a shell builtin"<<std::endl;
-      else std::cout<<arg<<": not found"<<std::endl;
+      if(arg=="echo " || arg=="exit " || arg=="type ") std::cout<<arg<<" is a shell builtin"<<std::endl;
+      std::string sys_path=std::getenv("PATH");
+      std::stringstream ss(sys_path);
+      std::string path;
+      bool found=false;
+      while(std::getline(ss,path,':'))
+      {
+        std::string full_path=path+'/'+arg;
+        if(access(full_path.c_str(),X_OK)==0)
+        {
+          std::cout<<arg<<" is "<<full_path<<std::endl;
+          found=true;
+          break;
+        }
+      }
+      if(!found) std::cout<<arg<<": not found"<<std::endl;
     }
     else std::cout<<command<<": command not found"<<std::endl;
 
