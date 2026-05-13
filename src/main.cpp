cf9fb1d9cfc10bc8dd6044cffde321bd43fe9716
Prav Prateek <prateekprav@gmail.com>
2026-05-13 09:13:13 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,13 @@ int main() {
     {
       std::cout<<command.substr(5)<<std::endl;
     }
+    else if(command.substr(0,5)=="type ")
+    {
+      std::string arg=command.substr(5);
+      if(arg=="echo") std::cout<<"echo is a shell builtin"<<std::endl;
+      else if(arg=="exit") std::cout<<"exit is a shell builtin"<<std::endl;
+      else std::cout<<arg<<": not found"<<std::endl;
+    }
     else std::cout<<command<<": command not found"<<std::endl;
 
   }
