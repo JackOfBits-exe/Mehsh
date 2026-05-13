351f18aa5cd0e2a94cf1d49487715c84a65e5701
Prav Prateek <prateekprav@gmail.com>
2026-05-13 08:36:41 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,12 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  while(true)
+  std::string command;
+  do
   {
     std::cout<<"$ ";
   
-    std::string command;
     std::getline(std::cin,command);
     std::cout<<command<<": command not found"<<std::endl;
-  }
+  } while(command!="exit");
 }
