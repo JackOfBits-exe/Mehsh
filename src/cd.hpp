f4b65ac4e6edb3edd8c0c5c54247ca9c3820f522
Prav Prateek <prateekprav@gmail.com>
2026-05-16 17:44:24 +0000

    codecrafters submit [skip ci]

--- src/cd.hpp
+++ src/cd.hpp
@@ -6,9 +6,28 @@
 
 namespace mehsh::cd
 {
+    std::pair<bool,std::string> handle_home(const std::string& args)
+    {
+        if(args.empty())
+        {
+            fs::path home_path=getenv("HOME");
+            return {true,home_path.string()};
+        }
+        else if(args[0]=='~')
+        {
+            fs::path home_path=getenv("HOME");
+            fs::path extended_path=args.substr(1);
+            return {true,(home_path/extended_path).string()};
+        }
+        return {false,args};
+    }
+
     int execute(const std::string& args)
     {
-        fs::path new_path=args;
+        fs::path new_path;
+        const auto [is_home,extended_args]=handle_home(args);
+        if(is_home) new_path=extended_args;
+        else new_path=args;
         if(fs::exists(new_path) && fs::is_directory(new_path))
         {
             fs::current_path(new_path);
